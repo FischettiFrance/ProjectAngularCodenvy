df394feb15e3c56f888710298be7cdab6fa42ba7
FischettiFrance <fischetti.francesco@iisgalvanimi.edu.it>
2019-10-18 11:01:13 +0000

    My First Commit

---
+++ client-angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'; //HTTP CLIENT
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  results: string[];
+  private BASE_URL:string = '/api';
+    // Inject HttpClient into your component or service.
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+      // Make the HTTP request:
+      this.http.get('https://3000-fd01ca2b-fe8b-445c-89b0-57a52fe81200.ws-eu0.gitpod.io/api').subscribe(data => {
+        // Read the result field from the JSON response.
+        this.results = data['results'];
+      });
+    }
+}
